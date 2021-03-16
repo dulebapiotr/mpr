777ac2646d8b946d8aa5b14adb6520644a771be1
duleba.pio <duleba.pio@gmail.com>
2021-03-16 21:07:11 +0100

    change step

---
+++ lab2/standard_communication.c
@@ -0,0 +1,49 @@
+#include "definitions.h"
+
+double sender(int msg_size) {
+    char *buff = malloc(msg_size);
+    int i;
+    double start = MPI_Wtime();
+    for (i = 0; i < N; i++) {
+        MPI_Send(buff, msg_size, MPI_BYTE, RECEIVER, 0, MPI_COMM_WORLD);
+        MPI_Recv(buff, msg_size, MPI_BYTE, RECEIVER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    return MPI_Wtime() - start;
+}
+
+void receiver(int msg_size) {
+    char *buff = malloc(msg_size);
+    int i;
+    for (i = 0; i < N; i++) {
+        MPI_Recv(buff, msg_size, MPI_BYTE, SENDER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(buff, msg_size, MPI_BYTE, SENDER, 0, MPI_COMM_WORLD);
+    }
+}
+
+void test(int rank) {
+    printf("N  msg_size  time \n");
+    int msg_size;
+    for (msg_size = 1; msg_size <= MAX_MESSAGE_SIZE; msg_size += 100) {
+        if (rank == SENDER) {
+            double time = sender(msg_size);
+            printf("%d  ", N);
+            printf("%d  ", msg_size);
+            printf("%f  \n", time);
+        } else if (rank == RECEIVER) {
+            receiver(msg_size);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    test(rank);
+
+    MPI_Finalize();
+    return 0;
+}
