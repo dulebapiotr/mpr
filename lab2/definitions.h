e2d2ea2372e8719f5e60667ddfe645b54a2217bd
duleba.pio <duleba.pio@gmail.com>
2021-03-16 18:00:37 +0100

    zad1

--- lab2/definitions.h
+++ lab2/definitions.h
@@ -5,11 +5,9 @@
 #include <mpi.h>
 #include <time.h>
 
-#define MIN_MESSAGE_SIZE 1
-#define MAX_MESSAGE_SIZE 100000
-#define MESSAGE_SIZE_INCREMENT_FACTOR 100
+#define SENDER 1
+#define RECEIVER 0
+#define N 1000
+#define MAX_SIZE 10000000
 
-#define TEST_ATTEMPTS 1000
-
-#define SENDER 0
-#define RECEIVER 1
+//MPIR_CVAR_CH3_NOLOCAL = 1;
--- lab2/single_node_shm_standard.c
+++ lab2/single_node_shm_standard.c
@@ -1,47 +1,45 @@
 #include "definitions.h"
 
-
 double sender(int size) {
     char *buff = malloc(size);
     int i;
     double start = MPI_Wtime();
-    for (i = 0; i < TEST_ATTEMPTS; i++) {
+    for (i = 0; i < N; i++) {
         MPI_Send(buff, size, MPI_BYTE, RECEIVER, 0, MPI_COMM_WORLD);
         MPI_Recv(buff, size, MPI_BYTE, RECEIVER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
     return MPI_Wtime() - start;
 }
 
-void receive(int size) {
+void receiver(int size) {
     char *buff = malloc(size);
     int i;
-    for (i = 0; i <TEST_ATTEMPTS; i++) {
+    for (i = 0; i < N; i++) {
         MPI_Recv(buff, size, MPI_BYTE, SENDER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Send(buff, size, MPI_BYTE, SENDER, 0, MPI_COMM_WORLD);
     }
 }
 
-void perform_tests(int rank){
-    int message_size;
-    for(message_size = MIN_MESSAGE_SIZE; message_size <= MAX_MESSAGE_SIZE; message_size *= MESSAGE_SIZE_INCREMENT_FACTOR){
-        if(rank == SENDER){
-            double test_time = send(message_size);
-            printf("%f", test_time);
-        } else if(rank == RECEIVER){
-            receive(message_size);
+void test(int rank) {
+    int size;
+    for (size = 1; size <= MAX_SIZE; size *= 2) {
+        if (rank == SENDER) {
+            double time = sender(size);
+            printf("%.5f\n", N*size/time/1000000*2);
+        } else if (rank == RECEIVER) {
+            receiver(size);
         }
     }
-
 }
 
+int main(int argc, char *argv[]) {
 
-int main(int argc, char *argv[]){
     MPI_Init(&argc, &argv);
 
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    perform_tests(rank);
+    test(rank);
 
     MPI_Finalize();
     return 0;
